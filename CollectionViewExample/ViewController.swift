08437620bd3048e0cd37d4d40091e0cc5c4d8786
Gonzalo Campos <gonzalocampose1@gmail.com>
2023-07-22 20:40:45 -0600

    Add crashing collection view.

--- CollectionViewExample/ViewController.swift
+++ CollectionViewExample/ViewController.swift
@@ -9,8 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let gridCollectionView: UICollectionView = {
+        let cv = UICollectionView()
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        return cv
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+
+        gridCollectionView.backgroundColor = .blue
+        view.addSubview(gridCollectionView)
+
+        NSLayoutConstraint.activate([
+            gridCollectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            gridCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            gridCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            gridCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+        ])
     }
 }
